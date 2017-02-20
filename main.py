9828902b6132b146ca42c0e385749fa825faa543
Agata Kargol <agata.kargol@zonarsystems.com>
2017-02-20 11:00:05 -0800

    type hinting on fixtures

--- main.py
+++ main.py
@@ -26,27 +26,18 @@ def index():
 @app.route('/<path:path>', methods=['GET', 'PUT', 'POST', 'PATCH', 'DELETE'])
 # @app.route('/<path:path>')
 def catch_all(path):
-    print('Got response')
     global REQUESTS
     global REQUESTS_LOCK
-    args = []
 
     if path == 'favicon.ico':
         return 'Not Found', 404
 
-    print('loc1')
-    print(request.is_json)
-
     if request.is_json:
-        print("JSON: {}".format(request.json))
         data = request.json
     else:
-        print(request.data)
         data = request.data
 
-    print('loc2')
     cr = CapturedRequest(str(uuid4()), path, dict(request.args), request.method, request.headers, data)
-    print("REQUEST: {}".format(cr))
 
     with REQUESTS_LOCK:
         REQUESTS.append(cr)
@@ -58,12 +49,9 @@ def catch_all(path):
     # TODO: currently this only checks the path and query args, we should add another parameter "method" so we can
     # allow users to specify different responses based on their request method of choice.
     potential_responses = RESPONSES.get(cr.path)
-    print("*****\npotential_responses: {}".format(potential_responses))
 
     if potential_responses is not None:
         for resp in potential_responses:
-            print("allowed args: {}".format(resp['args']))
-            print("got args: {}".format(cr.args))
             if resp['args'] == cr.args:
                 data = resp['response']
 
@@ -71,8 +59,6 @@ def catch_all(path):
                     return jsonify(**data)
                 else:
                     return data
-            else:
-                print('wrong args')
 
     return jsonify(success=True)
 
@@ -82,7 +68,6 @@ if __name__ == '__main__':
     parser.add_argument('port', help='The port to bind to')
     parser.add_argument('--responses', default=None, help='Path to responses file in JSON format.')
     args = parser.parse_args()
-    print('start')
 
     if args.responses:
         with open(args.responses) as f:
